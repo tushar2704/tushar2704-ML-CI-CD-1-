56e5407c5dda96ce246138c7d4645a64e73bc867
tushar2704 <tushar.inseec@gmail.com>
2023-04-07 11:54:06 +0530

    new

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,4 +20,24 @@ class DataIngestion:
         self.ingestion_config= DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        
+        logging,info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Lodaded the data as DataFrame')
+
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion od the data is completed")
+        except:
+            pass
