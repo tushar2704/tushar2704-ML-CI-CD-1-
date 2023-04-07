4e42272ad1c7ee08dedca9a0374ca0986fb94cb9
tushar2704 <tushar.inseec@gmail.com>
2023-04-07 12:18:09 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,7 +20,7 @@ class DataIngestion:
         self.ingestion_config= DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging,info("Entered the data ingestion method or component")
+        logging.info("Entered the data ingestion method or component")
         try:
             df = pd.read_csv('notebook\data\stud.csv')
             logging.info('Lodaded the data as DataFrame')
@@ -39,5 +39,16 @@ class DataIngestion:
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Ingestion od the data is completed")
-        except:
-            pass
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+if __name__ == "__main__":
+    obj =DataIngestion()
+    obj.initiate_data_ingestion()
