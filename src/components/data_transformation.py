bc94b5e31979095b28584a4eeff32dc48a920173
tushar2704 <tushar.inseec@gmail.com>
2023-04-08 17:32:10 +0530

    transform

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -27,6 +27,9 @@ class DataTranformation:
         self.data_transformation_config = DataTransformationConfig()
 
     def get_data_transformer_object(self):
+        '''
+        This function is responsible for Data Transformation
+        '''
         try:
             numerical_columns =['writing_score', 'reading_score']
             categorical_columns =[
@@ -71,4 +74,55 @@ class DataTranformation:
             return preprocessor
         except Exception as e:
             raise CustomException(e, sys)
+        
+
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df =pd.read_csv(train_path)
+            test_df =pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+            logging.info('Obtaining preprocessing object')
+
+            preprocessor_obj = self.get_data_transformer_object()
+
+            target_column_name ='math_score'
+            numerical_columns =['writing_score', 'reading_score']
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = test_df[target_column_name]
+
+            input_feature_test_df =test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df =test_df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe"
+            )
+
+            input_feature_train_arr=preprocessor_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr =preprocessor_obj.transform(input_feature_train_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)]
+            
+            test_arr = np.c_[
+                input_feature_test_arr np.array(target_feature_test_df)]
+            logging.info(f"saved preprocessing object")
+
+            save_object(
+                file_path =self.data_transformation_config.preprocessor_ob_file_path,
+                obj =preprocessor_obj)
+
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_ob_file_path,
+            )
+
+
+        except:
+            pass
+
 
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+
+def save_object(filepath, obj):
+    try:
+        dir_path = os.path.dirname(filepath)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(filepath 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
