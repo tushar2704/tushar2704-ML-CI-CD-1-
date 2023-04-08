a2eadf794dd702e36d880e81de6ee605dec38dd0
tushar2704 <tushar.inseec@gmail.com>
2023-04-08 16:47:29 +0530

    transform

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,6 @@
+# Import required
 import sys
+import os
 from dataclasses import dataclass
 
 import numpy as np
@@ -8,3 +10,51 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+# Exception & Logging
+from src.exception import CustomException
+from src.logger import logging
+
+# Data Transformation Class
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_ob_file_path =os.path.join('artifacts', "preprocessor.pkl")
+
+
+class DataTranformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns =['writing_score', 'reading_score']
+            categorical_columns =[
+                'gender', 
+                'race_ethnicity',
+                'parental_level_of_education',
+                'test_preparation_course'
+            ]
+
+            # Numerical Pipeline
+            num_pipeline =Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy='median')),
+                    ("scaler",StandardScaler())
+
+                ]
+            )
+
+            # Categorical Pipeline
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy='most_frequent') ),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler", StandardScaler())
+
+                ]
+            )
+        except:
+            pass
+
