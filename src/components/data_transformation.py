6ef4016a045dfd037d3a675a5f61a8c913539af7
tushar2704 <tushar.inseec@gmail.com>
2023-04-08 16:26:54 +0530

    transform

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -3,3 +3,8 @@ from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
